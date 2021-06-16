c522e447b2ea808957fccfec4b25c37aa3d9ffd4
MAXPOL <maxrol32@gmail.com>
2021-06-16 22:29:58 +0300

    Create sshClusterWithoutPassword.sh

--- sshClusterWithoutPassword.sh
+++ sshClusterWithoutPassword.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+ssh-keygen -t rsa -b 4096
+cd /root/.ssh
+
+# Next you need know ip address you server and use command ssh-copy-id 
+# Example ssh-copy-id root@192.168.0.10
+#add rows "ssh-copy-id" by number of servers
+#In process copy key you need enter password user from all servers
+
+ssh-copy-id username@ip-address 
